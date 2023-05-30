dc9fbef44a243106c02bd98e60d97ab3d8bddac7
AfroGit <94635472+AfroGit@users.noreply.github.com>
2023-05-30 17:01:39 +0000

    Created Container file and route.

---
+++ src/Container.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+
+// Should render a <div> element that wraps everything inside it.
+// Should have the class ui-container
+
+// This is an example of what wrapping means <Container><p>content</p></Container>. This would end up rendering: <div class="ui-container"><p>content</p></div>
+
+
+
+
+
